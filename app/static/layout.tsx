d7fa9a8f86e26756847488758274159f07edfb4b
VillageR88 <villager88pl@gmail.com>
2024-08-05 20:32:01 +0200

    intermediate deleted
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/layout.tsx
+++ app/static/layout.tsx
@@ -1,13 +1,16 @@
 /** @jsxImportSource preact */
+import { render } from 'preact';
 import Router from 'preact-router';
 import Home from './pages/page'; // Import Home component
 import About from './pages/about/page';
 import Location from './pages/location/page';
 import Careers from './pages/careers/page';
-
 import Navbar from './components/Navbar'; // Import Navbar component
+import '../static/css/globals.css'; // Import Tailwind CSS
 
-export function App() {
+document.body.className = 'min-h-screen w-full flex flex-col items-center justify-space-between';
+
+function App() {
   return (
     <>
       <Navbar />
@@ -23,3 +26,9 @@ export function App() {
     </>
   );
 }
+
+function Body() {
+  return <App />;
+}
+
+render(<Body />, document.body);
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,46 @@
-const path = require('node:path');
+import { resolve as _resolve } from 'node:path';
 
-module.exports = {
-    mode: 'development',
-    entry: './app/static/intermediate.tsx', // Adjust the entry point as needed
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'app/static/dist'),
+export const mode = 'development';
+export const entry = './app/static/layout.tsx';
+export const output = {
+  filename: 'bundle.js',
+  path: _resolve(__dirname, 'app/static/dist'),
+};
+export const resolve = {
+  extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  alias: {
+    // Add any necessary aliases here
+  },
+};
+export const module = {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-        alias: {
-            // Add any necessary aliases here
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            [
+              '@babel/preset-react',
+              {
+                pragma: 'h', // Preact pragma
+                pragmaFrag: 'Fragment', // Preact Fragment pragma
+              },
+            ],
+          ],
         },
+      },
     },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', ['@babel/preset-react', {
-                            pragma: 'h', // Preact pragma
-                            pragmaFrag: 'Fragment' // Preact Fragment pragma
-                        }]],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
-            },
-        ],
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
     },
+  ],
 };
