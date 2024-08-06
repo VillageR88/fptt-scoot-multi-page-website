98c5e163cd4a055013dda55ce01be35124c3e79b
VillageR88 <villager88pl@gmail.com>
2024-08-07 00:23:20 +0200

    Store buttons in footer
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/components/Footer.tsx
+++ app/static/pages/components/Footer.tsx
@@ -2,6 +2,26 @@ import IconFacebook from './IconFacebook';
 import IconTwitter from './IconTwitter';
 import IconInstagram from './IconInstagram';
 import NavbarBlock1 from './NavbarBlock1';
+import IconAppStore from '../../assets/icons/iconApple.svg';
+import IconGooglePlay from '../../assets/icons/iconGooglePlay.svg';
+import { Link } from 'preact-router';
+
+const storeIcons = [
+  {
+    imageSrc: IconAppStore as string,
+    key: 'app-store',
+    span: 'Available on the',
+    title: 'AppStore',
+    href: 'https://www.apple.com/app-store/',
+  },
+  {
+    imageSrc: IconGooglePlay as string,
+    key: 'google-play',
+    span: 'Get it on',
+    title: 'GooglePlay',
+    href: 'https://play.google.com/store',
+  },
+];
 
 const socialIcons = [
   { component: <IconFacebook />, key: 'facebook' },
@@ -16,11 +36,20 @@ export default function Footer() {
         <div className="mx-auto flex h-[96px] w-full max-w-[1100px] items-center justify-between">
           <h2 className="max-w-[415px] text-white">Sign up and Scoot off today</h2>
           <ul className="flex gap-[18px]">
-            {[1, 2].map((item) => (
-              <li key={item}>
-                <button type="button" className="buttonStore">
-                  {item}
-                </button>
+            {storeIcons.map((item) => (
+              <li key={item.key}>
+                <Link
+                  title={item.key}
+                  type="button"
+                  className="buttonStore flex items-center justify-center gap-[11px]"
+                  href={item.href}
+                >
+                  <img src={item.imageSrc} alt="" />
+                  <section className="flex flex-col text-left">
+                    <span className="text-[10px]">{item.span}</span>
+                    <h2 className="text-[20px] leading-[24px] tracking-[-0.89px] text-[#495567]">{item.title}</h2>
+                  </section>
+                </Link>
               </li>
             ))}
           </ul>
