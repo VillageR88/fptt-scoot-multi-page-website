de543c5b4ac1c6647f222e2b986665bfad33b0eb
VillageR88 <villager88pl@gmail.com>
2024-08-08 19:02:02 +0200

    initial Block1 of About
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/about/page.tsx
+++ app/static/pages/about/page.tsx
@@ -1,10 +1,12 @@
 import type { FunctionComponent } from 'preact';
 import InnerHeader from '../components/InnerHeader';
+import Block1 from './Block1';
 
 const About: FunctionComponent = ({ path }) => {
   return (
     <>
       <InnerHeader path={path} />
+      <Block1 />
     </>
   );
 };
