31215a6b0b6cfd5d4f502f8429cc62a3ba6b73b1
VillageR88 <villager88pl@gmail.com>
2024-08-09 21:24:39 +0200

    initial map pin
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/location/page.tsx
+++ app/static/pages/location/page.tsx
@@ -1,6 +1,7 @@
 import type { FunctionComponent } from 'preact';
 import InnerHeader from '../components/InnerHeader';
 import imageWorldMapDesktop from '../../assets/images/world-map-desktop.png';
+import patternTriangle from '../../assets/patterns/triangle.svg';
 import { Link } from 'preact-router';
 
 const items = {
@@ -15,8 +16,14 @@ const Location: FunctionComponent = ({ path }) => {
     <>
       <InnerHeader path={path} />
       <div className="my-[120px] flex w-full max-w-[1110px] flex-col gap-[120px]">
-        <div className="h-[543px] w-full">
-          <img src={imageWorldMapDesktop as string} alt="world map" />
+        <div className="relative h-[543px] w-full">
+          <div className="flex h-[88px] w-[189px] flex-col items-center">
+            <div className="pinBox relative z-[1] flex h-[72px] w-full items-center justify-center bg-[#FCB72B] font-mono text-[24px] font-bold leading-[28px] tracking-[-1.07px] text-[#495567]">
+              New York
+            </div>
+            <img className="relative z-[1]" src={patternTriangle as string} alt="" />
+          </div>
+          <img className="absolute top-0" src={imageWorldMapDesktop as string} alt="world map" />
         </div>
         <div className="flex min-h-[100px] w-full items-center justify-between">
           <section className="flex max-w-[826px] gap-[30px]">
