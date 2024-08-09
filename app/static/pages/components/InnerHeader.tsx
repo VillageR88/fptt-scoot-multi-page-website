1498420177933cb9f24598cb2b74ac8b57fdf7a4
VillageR88 <villager88pl@gmail.com>
2024-08-09 16:55:33 +0200

    logo change background
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/components/InnerHeader.tsx
+++ app/static/pages/components/InnerHeader.tsx
@@ -1,4 +1,5 @@
-import background from '../../assets/images/about-hero-desktop.jpg';
+import backgroundAbout from '../../assets/images/about-hero-desktop.jpg';
+import backgroundCareers from '../../assets/images/careers-locations-hero-desktop.jpg';
 import patternWhiteCircles from '../../assets/patterns/white-circles.svg';
 
 export default function InnerHeader({ path }: { path?: string }) {
@@ -12,9 +13,19 @@ export default function InnerHeader({ path }: { path?: string }) {
         return 'Careers';
     }
   }
+  function processPathToSrc(path: string) {
+    switch (path) {
+      case '/about':
+        return backgroundAbout as string;
+      case '/location':
+        return backgroundCareers as string;
+      case '/careers':
+        return backgroundCareers as string;
+    }
+  }
   return (
     <div className="relative flex h-[160px] w-full max-w-[1440px] items-center justify-center md:h-[200px]">
-      <img className="absolute h-full object-cover" src={background as string} alt="" />
+      <img className="absolute h-full object-cover" src={processPathToSrc(path ? path : '')} alt="" />
       <div className="mx-[32px] flex w-full max-w-[1106px] justify-center md:justify-start">
         <h1 className="relative">{processPathToTitle(path ? path : '')}</h1>
         <img className="absolute right-[-31px] hidden md:block" src={patternWhiteCircles as string} alt="" />
