bdf53236dc7919e8194ae6a2977ce55b76676d72
VillageR88 <villager88pl@gmail.com>
2024-08-09 18:03:34 +0200

    Block2 Careers
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/careers/page.tsx
+++ app/static/pages/careers/page.tsx
@@ -1,12 +1,14 @@
 import type { FunctionComponent } from 'preact';
 import InnerHeader from '../components/InnerHeader';
 import Block1 from './Block1';
+import Block2 from '../components/ValuesSection';
 
 const Careers: FunctionComponent = ({ path }) => {
   return (
     <>
       <InnerHeader path={path} />
       <Block1 />
+      <Block2 title="Why join us?" />
     </>
   );
 };
