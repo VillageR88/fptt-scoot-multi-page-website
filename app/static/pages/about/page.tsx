4c7d06dcbbdedbe95824562246118e50faa121f3
VillageR88 <villager88pl@gmail.com>
2024-08-09 17:20:28 +0200

    ValuesSection component
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/about/page.tsx
+++ app/static/pages/about/page.tsx
@@ -1,7 +1,7 @@
 import type { FunctionComponent } from 'preact';
 import InnerHeader from '../components/InnerHeader';
 import Block1 from './Block1';
-import Block2 from './Block2';
+import Block2 from '../components/ValuesSection';
 import Block3 from './Block3';
 
 const About: FunctionComponent = ({ path }) => {
@@ -9,7 +9,7 @@ const About: FunctionComponent = ({ path }) => {
     <>
       <InnerHeader path={path} />
       <Block1 />
-      <Block2 />
+      <Block2 title="Our values" />
       <Block3 />
     </>
   );
--- app/static/pages/components/ValuesSection.tsx
+++ app/static/pages/components/ValuesSection.tsx
@@ -22,10 +22,10 @@ const items = [
       'We support every community we serve. All workers are paid a living wage based on their location and are Scoot employees.',
   },
 ];
-export default function Block2() {
+export default function Block2({ title }: { title: string }) {
   return (
     <div className="mb-[145px] flex w-full max-w-[1110px] flex-col items-center gap-[48px] px-[32px] md:mb-[120px] md:px-0">
-      <h2 className="text-[#495567]">Our values</h2>
+      <h2 className="text-[#495567]">{title}</h2>
       <ul className="flex w-full flex-col items-center justify-between gap-[56px] xl:flex-row xl:gap-[30px]">
         {items.map((item, index) => (
           <li key={item.title} className="flex max-w-[350px] flex-col items-center">
